fb1c09bde4a7d2d6dd3d3e4c146ce1ec928f505e
Balaji <vijay.balaji.ext@suez.com>
2021-01-10 16:36:42 -0500

    initial commit

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,13 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+it('renders learn react link', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/learn react/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+it('renders learn react link', () => {
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
